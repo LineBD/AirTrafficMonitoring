29d96a3397ab96760df3575a4f26a9e45ccc1b57
kristinlieaas <kristinlieaas@gmail.com>
2018-04-03 09:42:24 +0200

    tilføjet konsoll++

---
+++ ConsoleAppATM/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TransponderReceiver;
+
+namespace ConsoleAppATM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                var myReciever = TransponderReceiverFactory.CreateTransponderDataReceiver();
+                myReciever.TransponderDataReady += MyReciever_TransponderDataReady;
+                Console.ReadKey();
+            }
+        }     
+                private static void MyReciever_TransponderDataReady(object sender, RawTransponderDataEventArgs e)
+                {
+                    var mylist = e.TransponderData;
+
+                    foreach (var track in mylist)
+                    {
+                        Console.WriteLine(track);
+                    }
+                }
+            }
+        }
