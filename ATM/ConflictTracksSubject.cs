3300166a3d3ff23df69bdbe86b1a295809b0347a
kristinlieaas <kristinlieaas@gmail.com>
2018-04-19 13:53:32 +0200

    ??

---
+++ ATM/ConflictTracksSubject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+  abstract  class ConflictTracksSubject
+    {
+        private List<IConflictTracks> _conflictList = new List<IConflictTracks>();
+        public void Attach(IConflictTracks conflicts)
+        {
+
+        }
+        public void Detach()
+        {
+
+        }
+        public void Notify()
+        {
+
+        }
+    }
+}
