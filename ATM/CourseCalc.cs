2c2348326e93ddbc316ae1c497db22b737a35a3c
LineBD <linebdanielsen@gmail.com>
2018-04-19 21:54:11 +0200

    CourseCAlc

--- ATM/CourseCalc.cs
+++ ATM/CourseCalc.cs
@@ -7,25 +7,24 @@ using System.Threading.Tasks;
 namespace ATM
 {
     public class CourseCalc: ICourseCalc
-
     {
-        private List<ITrack> _tracklist;
-
-        public double CalculateCourse(ITrack track1, ITrack track2)
+       // Mangler at få listen med...
+        public double CalculateCourse(ITrack oldtrack, ITrack newtrack)
         {
-            int x1coordinate = track1.XCoordinate;
-            int x2coordinate = track2.XCoordinate;
-            int y1coordinate = track1.YCoordinate;
-            int y2coordinate = track2.YCoordinate;
+            int x1coordinate = oldtrack.XCoordinate;
+            int x2coordinate = newtrack.XCoordinate;
+            int y1coordinate = oldtrack.YCoordinate;
+            int y2coordinate = newtrack.YCoordinate;
 
             int xCoordinate = x2coordinate - x1coordinate;
             int yCoordinate = y2coordinate - y1coordinate;
 
-            double theta = Convert.ToDouble(Math.Atan2(xCoordinate, yCoordinate));
+            double theta = Convert.ToDouble(Math.Atan2(yCoordinate,xCoordinate) - Math.Atan2(90000, 0)); 
             double CourseDegrees = theta * (180 / Math.PI);
             return CourseDegrees;
 
         }
+        //private List<ITrack> _tracklist;
         //public double CalculateCourses(List<ITrack> _tracklist)
         //{
         //    int x1coordinate = _tracklist[1].XCoordinate;
