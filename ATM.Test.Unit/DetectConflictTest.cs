d50f43b1af681a74469be579bbcf5ac4154aaaca
kristinlieaas <kristinlieaas@gmail.com>
2018-04-23 11:14:34 +0200

    Forsøg

--- ATM.Test.Unit/DetectConflictTest.cs
+++ ATM.Test.Unit/DetectConflictTest.cs
@@ -14,40 +14,66 @@ namespace ATM.Test.Unit
         private DetectConflict uut;
         private CheckCollision checkcollision;
         private IWrite alarm;
-        //private ILog log;
+        private IWrite log;
         [SetUp]
 
         public void SetUp()
         {
             // log = Substitute.For<ILog>();
-            checkcollision = Substitute.For<CheckCollision>();
-            alarm = Substitute.For<IWrite>();
-            uut = new DetectConflict(checkcollision);
+            //checkcollision = Substitute.For<CheckCollision>();
+            //alarm = Substitute.For<IWrite>();
+            //uut = new DetectConflict(checkcollision);
 
+            ////opretter to tracks med forskellig tag for at sammenligne de
+            //ITrack track1 = new Track();
+            //track1.Tag = "KLA";
+
+            //ITrack track2 = new Track();
+            //track2.Tag = "RIH";
+            ////Tilføjer de til liste
+            //checkcollision.ConflictingFlights.Add(track1);
+            //checkcollision.ConflictingFlights.Add(track2);
+            ////Opdaterer
+            //uut.Update();
+            log = Substitute.For<IWrite>();
+            checkcollision = Substitute.For<CheckCollision>();
+            uut = new DetectConflict(checkcollision);
             //opretter to tracks med forskellig tag for at sammenligne de
             ITrack track1 = new Track();
-            track1.Tag = "KLA";
+            track1.Tag = "KLA123";
+            track1.Timestamp = DateTime.ParseExact("20182304123456789", "yyyyMMddHHmmssfff", System.Globalization.CultureInfo.InvariantCulture);
+                
 
             ITrack track2 = new Track();
-            track2.Tag = "RIH";
-            //Tilføjer de til liste
-            checkcollision.ConflictingFlights.Add(track1);
-            checkcollision.ConflictingFlights.Add(track2);
-            //Opdaterer
-            uut.Update();
+            track2.Tag = "RIH456";
+            track1.Timestamp = DateTime.ParseExact("20182304123458789", "yyyyMMddHHmmssfff", System.Globalization.CultureInfo.InvariantCulture);
+
+        }
+
+        [Test]
+        public void WriteToFile_WriteBothTracks_Correct()
+        {
+            uut.Received().Wri
         }
 
             [Test]
 
-            public void UpdateList_WriteTrackOne_CorrectPrint()
+        public void UpdateList_WriteTrackOne_CorrectPrint()
         {
             alarm.Received().Write(Arg.Is<List<ITrack>>(trackList => trackList[0].Tag.Contains("KLA")));
         }
+        [Test]
         public void UpdateList_WriteTrackTwo_CorrectPrint()
         {
             alarm.Received().Write(Arg.Is<List<ITrack>>(trackList => trackList[1].Tag.Contains("RIH")));
         }
+        [Test]
+        public void Update_TimeStamp_CorrectYear()
+        {
+            var timeStamp = checkcollision.ConflictingFlights[0].Timestamp;
+            alarm.Received().Write(Arg.Is<List<ITrack>>(tracklist => tracklist[0].Timestamp.Equals(timeStamp)));
+        }
 
     }
-    }
-}
+
+   }
--- ATM/Output/ConflictAlarm.cs
+++ ATM/Output/ConflictAlarm.cs
@@ -10,7 +10,7 @@ namespace ATM
     {
         public void Write(List<ITrack> conflictingTracks)
         {
-            Console.WriteLine("ALARM - Conflicting flights! Flight: " + conflictingTracks[0].Tag + "and flight :" + conflictingTracks[1].Tag + "are in conflict." +
+            Console.WriteLine("ALARM!!! Conflicting flights! Flight: " + conflictingTracks[0].Tag + " and flight: " + conflictingTracks[1].Tag + " are in conflict." +
                 "\nTime of Occurence: " + conflictingTracks[0].Timestamp.Year + "/" + conflictingTracks[0].Timestamp.Month + "/" + conflictingTracks[0].Timestamp.Day +
                                  ", at " + conflictingTracks[0].Timestamp.Hour + ":" + conflictingTracks[0].Timestamp.Minute + ":" +
                                  conflictingTracks[0].Timestamp.Second + ":" + conflictingTracks[0].Timestamp.Millisecond);
