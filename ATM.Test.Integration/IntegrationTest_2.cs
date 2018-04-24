2ea2f1f64b4e8e973a60966b323a4a1183fadb78
kristinlieaas <kristinlieaas@gmail.com>
2018-04-24 09:51:45 +0200

    hh

--- ATM.Test.Integration/IntegrationTest_2.cs
+++ ATM.Test.Integration/IntegrationTest_2.cs
@@ -10,7 +10,7 @@ using TransponderReceiver;
 namespace ATM.Test.Integration
 {
     [TestFixture]
-   public class IntegrationTest_1
+    public class IntegrationTest_2
     {
         //Tester forbindelsen mellem controller display og trackparsing
 
@@ -18,7 +18,7 @@ namespace ATM.Test.Integration
         private ITrackParsing parseTracks;
         private ITrack track;
         private IFilterFlightLimits filter;
-        private ICompareTracks comparetracks;
+        private IConflictingTracks conflictingtracks;
         private CheckCollision collision;
         private IWrite write;
         private ITransponderReceiver receiver;
@@ -29,22 +29,14 @@ namespace ATM.Test.Integration
             track = new Track();
             parseTracks = new TrackParsing(track);
             write = Substitute.For<WriteToConsole>();
-            filter = Substitute.For<IFilterFlightLimits>();
+            filter = new FilterFlightLimits();
             collision = Substitute.For<CheckCollision>();
-            conmparetracks = Substitute.For<ICompareTracks>();
-            _mainreceiver= new MainReceiver(receiver, filter, write, collision, conflictingtracks, parseTracks);
+            comparetracks = Substitute.For<ICompareTracks>();
+            _mainreceiver = new MainReceiver(receiver, filter, write, collision, conflictingtracks, parseTracks);
 
-        }
-       
         [Test]
-    
-        public void FilterCreatedFlight_FromString_Correct()
-        {
-            string _flight = "TRK042;13000;130000;13000;20180403100622937";
-            _mainreceiver.MyReceiver_TransponderDataReady(this, new RawTransponderDataEventArgs(new List<string> { _flight }));
+        public void CompareTracks_UpdateTracks
 
-            ITrack track = parseTracks.CreateFlight(_flight);
-            filter.Received().Filtering(track);
-        }
+
     }
 }
